3d9beb8bb06ab565624f72e25978dd8941e406df
{shiva} <girishiva93@gmail.com>
2022-02-07 16:45:54 +0545

    React Project

---
+++ setup/src/tours/Loading.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Tours = () => {
+  return <h1>It's Loading...</h1>;
+};
+
+export default Tours;
---
+++ setup/src/tours/Tours.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Tour from "./Tour";
+
+const Tours = ({ tours, removeItem }) => {
+  return (
+    <section>
+      <div className="title">
+        <h2>our tours</h2>
+        <div className="underline"></div>
+      </div>
+      <div>
+        {tours.map((tour) => {
+          return <Tour key={tour.id} {...tour} removeItem={removeItem} />;
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default Tours;
